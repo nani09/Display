77f749bef277038989a7404d63c3e3882cc19c5d
ravishankar09 <ravishankar09@thirdleap.ai>
2019-07-07 20:44:43 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'medcords';
+  constructor(private router: Router) {
+    this.router.navigate(['']);
+  }
+  title = 'Display Templets';
 }
